5373eb03cb76d586f9f0099abdaf2546947601a9
eskaigarcia <yosoyeskai@gmail.com>
2025-02-12 08:23:58 +0100

    Revisión keyboard handler

--- keyboardHandler.js
+++ keyboardHandler.js
@@ -2,75 +2,38 @@ document.addEventListener('keydown', function(event) {
     console.log(event);
 });
 
-var positionLeft = 20;
-var positionTop= 20;
-var distance = 5;
-var player = document.getElementById('player');
-var playersTalk = document.getElementById('playersTalk');
-var screenW = window.innerWidth;
-var screenH = window.innerHeight;
-
 
 function keyboardHandler (event){
     switch(event.KeyCode){
-        case 37:                 //Flecha izquierda
-            if (positionLeft>0) {
-                positionLeft-=distance;
-                player.style.left = positionLeft + 'px';
-            }
-        break
-        case 39: 
-        if (positionLeft+distance <= screenW - 50)   //Flecha derecha
-            positionLeft += distance;
-            player.style.left = positionLeft + "px";
-        break;
-          
-        case 13:                    //enter salto
-        if (positionTop>0) {
-            positionTop-=distance;
-            player.style.top = positionTop + 'px';
-        }
-        break
-        case 40:                    // ir hacia abajo
-        if (positionTop + distance <= screenH - 80) {
-            positionTop += distance;
-            player.style.top = positionTop + 'px';
-        }
-        break 
-        case 76:                    //Dialogo personaje
-            playersTalk.style.display = "block";
-        }
-        
-    }
+        case 37:                    // Flecha izquierda
 
-    function keyboardHandler (event){
-        switch(event.KeyCode){
-            case 65:                 //Tecla A
-                if (positionLeft>0) {
-                    positionLeft-=distance;
-                    player.style.left = positionLeft + 'px';
-                }
-            break
-            case 68: 
-            if (positionLeft+distance <= screenW - 50)   //Tecla D
-                positionLeft += distance;
-                player.style.left = positionLeft + "px";
             break;
-              
-            case 32:                    //barra espaciadora salto
-            if (positionTop>0) {
-                positionTop-=distance;
-                player.style.top = positionTop + 'px';
-            }
-            break
-            case 83:                    // ir hacia abajo
-            if (positionTop + distance <= screenH - 80) {
-                positionTop += distance;
-                player.style.top = positionTop + 'px';
-            }
-            break 
-            case 70:                    //Dialogo personaje
-                playersTalk.style.display = "block";
-            }
-            
+        case 39:                    // Flecha derecha
+
+            break;
+        case 13:                    // Flecha arriba
+
+            break;
+        case 40:                    // Flecha abajo
+
+            break;
+        case 76:                    // L
+
+            break;
+        case 65:                    // A
+
+            break;
+        case 68:                    // D
+
+            break;
+        case 32:                    // Espacio
+
+            break;
+        case 83:                    // S
+
+            break;
+        case 70:                    // F
+        
+            break;
         }
+    }
--- tareas.md
+++ tareas.md
@@ -1,11 +1,11 @@
 [ ] Diseños - Desi
 [ ] Pagina de inicio - Desi
 
-[ ] Colocar nuevos personajes evitando colisiones - Eskai
+[-] Colocar nuevos personajes evitando colisiones - Eskai
 [ ] Mover personajes por teclado (Izquierda y derecha) - Desi
 [ ] Mover personajes por teclado (Salto) 
 [ ] Mover personajes por tecalado (Agachar) *opcional - Desi
-[ ] Gravedad - Eskai
+[X] Gravedad - Eskai
 [ ] Evitar colisiones - Eskai
 
 [ ] Diálogos - Desi

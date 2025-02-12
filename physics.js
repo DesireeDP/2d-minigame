b3f7c07ebfeda5ca52e718135ee3d8f74ac13343
eskaigarcia <yosoyeskai@gmail.com>
2025-02-12 18:10:54 +0100

    Full collider proof of concept

--- physics.js
+++ physics.js
@@ -8,22 +8,47 @@ function physics() {
         else if(playerData[i].gravity < 20) playerData[i].gravity += 0.4;
     }
     for(i=0; i<playerData.length; i++) {
+        let prevPosition = playerData[i].bottom;
         if(playerData[i].bottom <= 60 && playerData[i].gravity > 0) {
             playerData[i].gravity = 0;
             playerData[i].bottom = 60;
         } else {
             playerData[i].bottom = Math.min(playerData[i]. bottom - playerData[i].gravity, (window.innerHeight - innerData.characterHeight));
         }
+        if (getCollisions(i)) playerData[i].bottom = prevPosition;
     }
 
     // Aceleración lateral
     moveCharacters();
     for(i=0; i<playerData.length; i++) {
+        let prevPosition = playerData[i].left;
         playerData[i].left += playerData[i].accel;
         if (playerData[i].left < 0) playerData[i].left = 0;
         if (playerData[i].left > window.innerWidth - innerData.characterWidth) playerData[i].left = window.innerWidth - innerData.characterWidth;
+        if (getCollisions(i)) playerData[i].left = prevPosition;
     }
 }
+
+// Comprobar colisiones en la coordenada X
+function getCollisions(p) {
+    let contact = false;
+    let deltaX = 0, deltaY = 0;
+    for (j=0; j<playerData.length; j++) {
+        if(j != p) {
+            deltaX = playerData[j].left - playerData[p].left;
+            if (deltaX < innerData.characterWidth && deltaX > - innerData.characterWidth) {
+                deltaY = playerData[j].bottom - playerData[p].bottom;
+                if (deltaY < innerData.characterHeight && deltaY > - innerData.characterHeight) {
+                    contact = true;
+                }
+            }
+        }
+    }
+    return contact;
+
+}
+
+
 
 // Ejecutar fuerza de salto
 function jump(player2 = false) {
